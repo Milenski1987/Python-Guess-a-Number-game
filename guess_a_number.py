5bb9c719b6f5a1daae8c4eec6078368e1b78e5a3
Milen Nikolov <milenski1987@icloud.com>
2025-01-26 22:34:08 +0200

    added difficulty feature in game

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,14 +1,38 @@
 import random
 
 
-number_to_guess = random.randint(1, 100) #computer set random number
-tries = 0
 print("Wellcome to Guess a Number mini game. Try to guess a random number between 1 and 100 in 10 tries")
 
-while tries < 10:
-    players_guess = input("Guess the number from 1 to 100: ") #read player's guess
+number_limit_based_on_difficulty = 0
 
-    if int(players_guess) not in range(1, 101): #check player's guess for range of input
+while True: #user can choose difficulty of the game
+    print("Choose difficulty")
+    print("Easy: 10 tries and number range 1 - 100")
+    print("Medium: 10 tries and number range 1 - 500")
+    print("Hard: 10 tries and number range 1 - 1000")
+    difficulty = input("Please select Easy, Medium or Hard: ")
+
+    if difficulty == "Easy":
+        number_limit_based_on_difficulty = 100
+        break
+    elif difficulty == "Medium":
+        number_limit_based_on_difficulty = 500
+        break
+    elif difficulty == "Hard":
+        number_limit_based_on_difficulty = 1000
+        break
+    else:
+        print("Invalid selection. Try again...")
+
+
+number_to_guess = random.randint(1, number_limit_based_on_difficulty) #computer set random number
+tries = 0
+
+
+while tries < 10:
+    players_guess = input(f"Guess the number from 1 to {number_limit_based_on_difficulty}: ") #read player's guess
+
+    if int(players_guess) not in range(1, number_limit_based_on_difficulty + 1): #check player's guess for range of input
         print("Your number is out of range.")
         continue
 
@@ -19,7 +43,7 @@ while tries < 10:
     players_guess = int(players_guess)
 
     if players_guess == number_to_guess:
-        print("You guess it and won the game")
+        print("You guess it and won the game!")
         break
 
     elif players_guess > number_to_guess:
