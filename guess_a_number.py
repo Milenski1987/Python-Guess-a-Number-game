1b71e74f3da88a81f0fb1fe71a07fd68d7c961c5
Milen Nikolov <milenski1987@icloud.com>
2025-01-26 22:45:10 +0200

    added feature to chose number of tries

--- guess_a_number.py
+++ guess_a_number.py
@@ -4,6 +4,8 @@ import random
 print("Wellcome to Guess a Number mini game. Try to guess a random number between 1 and 100 in 10 tries")
 
 number_limit_based_on_difficulty = 0
+tries = 0
+tries_count = 0
 
 while True: #user can choose difficulty of the game
     print("Choose difficulty")
@@ -24,12 +26,19 @@ while True: #user can choose difficulty of the game
     else:
         print("Invalid selection. Try again...")
 
+while True: #user can choose number of tries
+    how_many_tries = input("How many tries you want?: ")
+    if how_many_tries.isdigit() and int(how_many_tries) > 0:
+        tries = int(how_many_tries)
+        break
+    else:
+        print("Invalid input. Try again...")
+
 
 number_to_guess = random.randint(1, number_limit_based_on_difficulty) #computer set random number
-tries = 0
 
 
-while tries < 10:
+while tries_count < tries:
     players_guess = input(f"Guess the number from 1 to {number_limit_based_on_difficulty}: ") #read player's guess
 
     if int(players_guess) not in range(1, number_limit_based_on_difficulty + 1): #check player's guess for range of input
@@ -52,7 +61,7 @@ while tries < 10:
     elif players_guess < number_to_guess:
         print("Your number is lower!")
 
-    tries += 1
+    tries_count += 1
 
 else:
     print("Sorry, no more tries. You lost")
